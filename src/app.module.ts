e66203aefd0447660c50590d599fc912af7e6fae
Erick Saravia <ericksaravia16@gmail.com>
2024-08-17 16:41:47 -0600

    refactor(app.module): Removed unused app.controller and app.service

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,6 @@ import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { APP_FILTER, APP_GUARD } from '@nestjs/core';
 import { ConfigModule } from '@nestjs/config';
 
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { CatsModule } from './modules/cats/cats.module';
 import { HttpExceptionFilter } from './common/filters/httpException.filter';
 import { RolesGuard } from './common/guards/roles.guard';
@@ -20,9 +18,8 @@ import { DatabaseModule } from './core/database/database.module';
     }),
     DatabaseModule,
   ],
-  controllers: [AppController],
+  controllers: [],
   providers: [
-    AppService,
     {
       provide: APP_FILTER,
       useClass: HttpExceptionFilter,
